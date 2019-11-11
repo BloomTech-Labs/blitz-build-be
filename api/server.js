586a85a9e10b8065dd0d820c88f1553f421afe3a
Mike Harley <mharley12345@gmail.com>
2019-11-11 09:40:10 -0800

    adds add task functionality

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 
 const projectsRouter = require('../routes/projects-routes')
-
+const taskRouter = require('../routes/tasks-routes')
 const register = require('../auth/authRouter')
 const login = require('../auth/authRouter')
 const server = express()
@@ -25,6 +25,7 @@ server.use('/api',login,register)
 
 // Add auth to routes
 server.use('/api/auth',projectsRouter)
+server.use('/api/auth/:uid/projects',taskRouter)
 
 
 
