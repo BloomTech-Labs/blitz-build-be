bbee8bf9ada7b4255886b8180b231c82a275691e
Mike Harley <mharley12345@gmail.com>
2019-11-14 13:51:51 -0800

    adds all curd to tasks endpoints

---
+++ routes/tasks-router.js
@@ -0,0 +1,303 @@
+const router = require('express').Router();
+const Firebaseconfig = require('../Firebaseconfig')
+const dbRef = Firebaseconfig.database().ref()
+const moment = require('moment')
+
+
+/* Route /api/auth/:uid/projects/:projectID/tasks  */
+
+//Get all tasks for a project
+router.get('/:uid/projects/:projectID/tasks', async (req, res) => {
+
+    let uid = req.params.uid
+    let projectID = req.params.projectID
+
+    const tasksRef = dbRef.child(`/${uid}/projects/${projectID}/tasks`);
+
+    await tasksRef.on("value", tasksObj => {
+
+        //  console.log(templateObj.val())
+
+        let tasks = tasksObj.val()
+
+
+
+
+        try {
+
+            if (tasks) {
+
+                res.status(200)
+                    .json(tasks)
+
+            }
+
+        }
+
+        catch
+
+        (err) {
+            res.status(500)
+
+                .json(
+                    {
+                        message: err.message
+                    }
+                )
+        }
+
+
+
+    })
+
+})
+
+// Get tasks by filter 
+router.get('/:uid/projects/:projectID/tasks/:filter', async (req, res) => {
+    let filter = req.params.filter
+    let uid = req.params.uid
+    let projectID = req.params.projectID
+
+    const tasksRef = dbRef.child(`/${uid}/projects/${projectID}/tasks`);
+
+    await tasksRef.orderByChild(`tasks/${filter}`)
+    .on("value",tasksObj => {
+
+        //  console.log(templateObj.val())
+
+        let tasks = tasksObj.val()
+
+
+
+
+        try {
+
+            if (tasks) {
+
+                res.status(200)
+                    .json(tasks)
+
+            }
+
+        }
+
+        catch
+
+        (err) {
+            res.status(500)
+
+                .json(
+                    {
+                        message: err.message
+                    }
+                )
+        }
+
+
+
+    })
+
+})
+router.put('/:uid/projects/:projectID/tasks/:taskID', async (req,res)=>{
+    let uid = req.params.uid
+    let body = req.body
+    let projectID = req.params.projectID
+    let taskID= req.params.taskID
+    let tasksRef = dbRef.child(`/${uid}/projects/${projectID}/tasks/${taskID}`)
+    let taskRef = dbRef.child(`/${uid}/tasks/${taskID}`)
+ 
+      tasksRef.update(body)
+
+      tasksRef.on("value",updateObj =>{
+        let updates = updateObj.val()
+        
+      taskRef.update(updates)
+        try {if (updates) {
+
+                res.status(200)
+                    .json(updates)
+
+            }
+        }
+      catch
+          (err) {
+            res.status(500)
+                .json(
+                    {
+                        message: err.message
+                    }
+                )
+        }
+    })
+    })
+
+router.get('/:uid/tasks', async (req, res) => {
+
+    let uid = req.params.uid
+   
+
+    const tasksRef = dbRef.child(`/${uid}/tasks`);
+     
+    await tasksRef.on("value", tasksObj => {
+
+        //  console.log(templateObj.val())
+
+        let tasks = tasksObj.val()
+
+
+
+
+        try {
+
+            if (tasks) {
+
+                res.status(200)
+                    .json(tasks)
+
+            }
+
+        }
+
+        catch
+
+        (err) {
+            res.status(500)
+
+                .json(
+                    {
+                        message: err.message
+                    }
+                )
+        }
+
+
+
+    })
+
+})
+router.get('/:uid/projects/:projectID/:taskID', async (req, res) => {
+
+    let uid = req.params.uid
+    let projectID = req.params.projectID
+    let taskID = req.params.taskID
+    const tasksRef = dbRef.child(`/${uid}/projects/${projectID}/tasks/${taskID}`);
+
+    await tasksRef.on("value", tasksObj => {
+
+        //  console.log(templateObj.val())
+
+        let tasks = tasksObj.val()
+
+
+
+
+        try {
+
+            if (tasks) {
+
+                res.status(200)
+                    .json(tasks)
+
+            }
+
+        }
+
+        catch
+
+        (err) {
+            res.status(500)
+
+                .json(
+                    {
+                        message: err.message
+                    }
+                )
+        }
+
+
+
+    })
+
+})
+
+router.post('/:uid/projects/:projectID/tasks', async (req, res) => {
+    
+    let body = req.body
+    let uid = req.params.uid
+    let projectID = req.params.projectID
+    let task_name = body.task_name
+    let due_date = req.body.due_date
+    let task_description = req.body.task_description
+   
+
+    await dbRef.child(`/${uid}/projects/${projectID}/tasks`).push(
+
+         
+
+
+        { due_date: due_date, projectID:projectID,task_description: task_description, task_name: task_name,isComplete:false}
+
+
+
+    )
+
+    const tasksRef = dbRef.child(`/${uid}/projects/${projectID}/tasks`);
+      let newTasksRef = tasksRef.push()
+      let taskID = newTasksRef.key
+    console.log(taskID)
+    tasksRef.on("value", tasksObj => {
+
+        //  console.log(templateObj.val())
+
+        let tasks = tasksObj.val()
+
+      
+     dbRef.child(`/${uid}/tasks`).update(tasks)
+
+        try {
+
+            if (tasks) {
+
+                res.status(201)
+                    .json({ message: `Task ${taskID} createdAT: ${moment().format('LLL')}`, tasksObj: tasks })
+
+            }
+
+        }
+
+        catch
+
+        (err) {
+            res.status(500)
+
+                .json(
+                    {
+                        message: err.message
+                    }
+                )
+        }
+
+
+
+    })
+
+})
+//Delete Tasks
+
+router.delete('/:uid/projects/:projectID/tasks/:taskID', async (req,res)=>{
+    let uid = req.params.uid
+    let body = req.body
+    let projectID = req.params.projectID
+    let taskID= req.params.taskID
+    let tasksRef = dbRef.child(`/${uid}/projects/${projectID}/tasks/${taskID}`)
+    let taskRef = dbRef.child(`/${uid}/tasks/${taskID}`)
+ 
+    
+       tasksRef.remove()
+       taskRef.remove()
+     .then(delObj=>{
+         res.status(200).json({message:`${taskID} deleted ${moment().format('LLL')}`,delObj})
+     })
+    })
+
+module.exports = router
