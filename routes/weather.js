306a822d122cea375b437263ca3e7a5f7cf0fbe5
Mike Harley <mharley12345@gmail.com>
2019-11-15 00:55:02 -0800

    adds search by zipcode for weather

--- routes/weather.js
+++ routes/weather.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const router = require('express').Router()
 const axios = require('axios')
+const zipcodes = require('zipcodes')
 router.post('/weather', (req, res) => {
     const api_key = process.env.WEATHER_API_KEY;
     const latitude = req.body.latitude;
@@ -16,5 +17,44 @@ router.post('/weather', (req, res) => {
           console.log(err);
         });
  })
+ router.get('/weather/:zipcode', (req, res) => {
+  const api_key = process.env.WEATHER_API_KEY;
+  const body = req.body
+  const zipcode = req.params.zipcode
+const zipCodes = zipcodes.lookup(req.params.zipcode);
+let latitude = zipCodes.latitude;
+let longitude = zipCodes.longitude
 
+
+    axios
+      .get(
+        `https://api.darksky.net/forecast/${api_key}/${latitude},${longitude}`
+      )
+      .then(response => {
+        res.status(200).json(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+})
+router.get('/weather/:zipcode', (req, res) => {
+  const api_key = process.env.WEATHER_API_KEY;
+  const body = req.body
+  const zipcode = req.params.zipcode
+const zipCodes = zipcodes.lookup(req.params.zipcode);
+let latitude = zipCodes.latitude;
+let longitude = zipCodes.longitude
+
+
+    axios
+      .get(
+        `https://api.darksky.net/forecast/${api_key}/${latitude},${longitude}`
+      )
+      .then(response => {
+        res.status(200).json(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+})
  module.exports = router
