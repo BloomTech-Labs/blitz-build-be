d7b122d0f3aa4d46362ecb4fec8fd93ff20b1578
Mike Harley <mharley12345@gmail.com>
2019-11-15 10:27:14 -0800

    .

--- auth/validate.js
+++ auth/validate.js
@@ -3,14 +3,17 @@ const admin = require('./Firebase-admin')
 async function verify(req,res,next){
     const token = req.headers.token
 
+
     try {
         const decodedToken = await admin.auth().verifyIdToken(token);
 
         if(decodedToken) {
-            req.body.uid = decodedToken.uid
+            // req.body.uid = decodedToken.uid
+      
+     
             return next()
         }else{
-            return res.status(401).json({message:"Please Log In",uid:decodedToken.uid})
+            return res.status(401).json({message:"Please Log In"})
         }
     } catch (err){
         return res.status(500).json({message:err.message})
