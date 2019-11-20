d6356cb825547a71b231fc3ffd47656325c452e3
Connor Holly <connorholly11@gmail.com>
2019-11-19 17:31:17 -0700

    working on weather

--- data/weather/weather.router.js
+++ data/weather/weather.router.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const router = require("express").Router();
 const axios = require("axios");
 const zipcodes = require("zipcodes");
-router.post("/weather", (req, res) => {
+
+router.post("/", (req, res) => {
   const api_key = process.env.WEATHER_API_KEY;
   const latitude = req.body.latitude;
   const longitude = req.body.longitude;
@@ -15,7 +16,8 @@ router.post("/weather", (req, res) => {
       console.log(err);
     });
 });
-router.get("/weather/:zipcode", (req, res) => {
+
+router.get("/:zipcode", (req, res) => {
   const api_key = process.env.WEATHER_API_KEY;
   const body = req.body;
   const zipcode = req.params.zipcode;
@@ -32,22 +34,22 @@ router.get("/weather/:zipcode", (req, res) => {
       console.log(err);
     });
 });
-router.get("/weather/:zipcode", (req, res) => {
-  const api_key = process.env.WEATHER_API_KEY;
-  const body = req.body;
-  const zipcode = req.params.zipcode;
-  const zipCodes = zipcodes.lookup(req.params.zipcode);
-  let latitude = zipCodes.latitude;
-  let longitude = zipCodes.longitude;
+// router.get("/weather/:zipcode", (req, res) => {
+//   const api_key = process.env.WEATHER_API_KEY;
+//   const body = req.body;
+//   const zipcode = req.params.zipcode;
+//   const zipCodes = zipcodes.lookup(req.params.zipcode);
+//   let latitude = zipCodes.latitude;
+//   let longitude = zipCodes.longitude;
 
-  axios
-    .get(`https://api.darksky.net/forecast/${api_key}/${latitude},${longitude}`)
-    .then(response => {
-      res.status(200).json(response.data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
+//   axios
+//     .get(`https://api.darksky.net/forecast/${api_key}/${latitude},${longitude}`)
+//     .then(response => {
+//       res.status(200).json(response.data);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+// });
 
 module.exports = router;
