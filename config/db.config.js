6af4d1a1c34abf803e10434ded8482193fb1e83a
Mike Harley <mharley12345@gmail.com>
2019-11-22 12:26:48 -0800

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,10 @@
 require('dotenv').config('./env')
+const client = require('pg')
 const knex = require('knex')('production')
 
 
 
 const knexConfig = require("../knexfile");
 
-CLIENT=postgres
+
 module.exports = knex(knexConfig.production);
