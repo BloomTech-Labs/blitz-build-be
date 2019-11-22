ecaf10331e8c5ea9c6c9fd33f4da18bc8e93f563
Frank Martinez <frankie.martinez@gmail.com>
2019-11-22 13:35:18 -0800

    🚧 Fixingf commit issue

--- data/users/users-router.js
+++ data/users/users-router.js
@@ -7,20 +7,12 @@ const jwt = require("jsonwebtoken");
 const secrets = require("../../config/secret.config");
 
 // for endpoints beginning with /users
-<<<<<<< HEAD
 router.post('/register', (req, res) => {
   let user = req.body
    console.log(req.user)
   const hash = bcrypt.hashSync(user.password,10); // 2 ^ n
  user.password = hash
 
-=======
-router.post("/register", (req, res) => {
-  let user = req.body;
-  // console.log(user)
-  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
-  user.password = hash;
->>>>>>> 191902a68ed8e7aa45ccc7cb6c7f2285dc3888b4
 
   db.add(user)
     .then(saved => {
@@ -34,7 +26,6 @@ router.post("/register", (req, res) => {
       res.status(500).json({ message: "cannot add the user", error });
     });
 });
-<<<<<<< HEAD
 router.put('/:id', (req, res) => {
   const username = req.params.username;
   const changes = req.body;
@@ -48,22 +39,6 @@ router.put('/:id', (req, res) => {
                }else{ res.status(404).json({ error: "Please enter a valid password" })
           }})
           .catch(err => res.status(500).json({ error: err }));
-=======
-router.put("/username/update", (req, res) => {
-  const username = req.params.username;
-  const changes = req.body;
-
-  if (changes) {
-    Receipts.updateReceipt(username, changes)
-      .then(count => {
-        if (count) {
-          res.status(202).json(count);
-        } else {
-          res.status(404).json({ error: "Please enter a valid password" });
-        }
-      })
-      .catch(err => res.status(500).json({ error: err }));
->>>>>>> 191902a68ed8e7aa45ccc7cb6c7f2285dc3888b4
   } else {
     res.status(400).json({ error: "Please provide all required fields." });
   }
@@ -71,7 +46,6 @@ router.put("/username/update", (req, res) => {
 
 router.post("/login", (req, res) => {
   let { username, password } = req.body;
-<<<<<<< HEAD
 
 
     if(!username){
@@ -111,48 +85,6 @@ router.post("/login", (req, res) => {
 //     res.status(200).json({message:'Not logged in'})
 //   }
 // })
-=======
-
-  // if(!username || !password){
-
-  //   res.status(403).json({message:'Please enter login information'})
-  // }
-  if ({ username, password }) {
-    db.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          // produce token
-          const token = generateToken(user);
-
-          // add token to response
-          res.status(200).json({
-            message: `Welcome ${user.username}!`,
-            token
-          });
-        } else {
-          res.status(404).json({ message: "User does not exist" });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
-  } else {
-    res.status(401).json({ message: "invalid credentials" });
-  }
-});
-router.get("/logout", (req, res) => {
-  if (req.session) {
-    req.session.destroy(err => {
-      res.status(200).json({
-        message: "Logout successfull"
-      });
-    });
-  } else {
-    res.status(200).json({ message: "Not logged in" });
-  }
-});
->>>>>>> 191902a68ed8e7aa45ccc7cb6c7f2285dc3888b4
 // users/
 router.put("/:id", async (req, res) => {
   const id = req.params.id;
