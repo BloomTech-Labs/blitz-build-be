2d6789a2acb73dcadb61f52ad47f1ecd743eb3cd
Mike Harley <mharley12345@gmail.com>
2019-11-29 23:16:48 -0800

    fixes updatedAt

--- data/delay-logs/delay_logs.model.js
+++ data/delay-logs/delay_logs.model.js
@@ -18,7 +18,8 @@ function addLogs(newLog){
     return db("delay_logs").insert({createdAt:createdAt},newLog);
 }
 function editLogs(id,changes){
-  return db("delay_logs").where({id}).update({updated:updatedAt},changes)
+  return db("delay_logs").where({id}).update({...
+        changes,updatedAt:moment().format("LLL")})
 }
 function deleteLogs(id){
     return db("delay_logs").where({id}).delete()
