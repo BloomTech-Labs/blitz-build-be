0a669b45c7f95c66804c2b83d3df2aa0b06dbe48
Mike Harley <mharley12345@gmail.com>
2019-11-30 00:53:36 -0800

    .

--- data/seeds/005-templates.js
+++ data/seeds/005-templates.js
@@ -8,73 +8,79 @@ exports.seed = function(knex) {
   //   .then(function() {
   //     // Inserts seed entries
   return knex("templates").insert([
-    { template_name: "90_day"},
-   { "tasks": [{
-  
+    {
+       "id":1,
+      "template_name":"90 Day",
         "task_number":1,
-        "due_date": `${date}`,
+        "due_date": date,
         "task_name": "Order Scrap Dumpster and Port-A-John",
         "isComplete": false,
-        "createdAt": `${createdAt}`
+        "createdAt": moment().format("LLL")
     
       },
       {
-      
+        "id":2,
+        "template_name":"90 Day",
         "task_number":2,
-        "due_date": `${date}`,
+        "due_date": date,
         "task_name": "Pre Construction Meeting With Buyers (Start Forming)",
         "isComplete": false,
-        "createdAt": `${createdAt}`
+        "createdAt":  moment().format("LLL")
   
       },
       { 
-      
+        "id":3,
+        "template_name":"90 Day",
         "task_number":3,
-        "due_date": `${date}`,
+        "due_date": date,
         "task_name": "Form Board Survey",
         "isComplete": false,
-        "createdAt": `${createdAt}`
+        "createdAt":  moment().format("LLL")
   
       },
       {
-    
+        "id":4,
+        "template_name":"90 Day",
         "task_number":4,
-        "due_date": `${date}`,
+        "due_date": date,
         "task_name": "Fill The Form Boards ",
         "isComplete": false,
-        "createdAt": `${createdAt}`
+        "createdAt":  moment().format("LLL")
    
       },
       {
-      
+        "id":5,
+        "template_name":"90 Day",
         "task_number":5,
-        "due_date": `${date}`,
+        "due_date": date,
         "task_name": "Rough Plumbing",
         "isComplete": false,
-        "createdAt": `${createdAt}`
+        "createdAt":  moment().format("LLL")
      
       },
       {
-      
+        "id":6,
+        "template_name":"90 Day",
         "task_number":6,
-        "due_date": `${date}`,
+        "due_date": date,
         "task_name": "Sewer Hookup / Order Windows",
         "isComplete": false,
-        "createdAt": `${createdAt}`
+        "createdAt":  moment().format("LLL")
        
       },
       {
-     
+        "id":7,
+        "template_name":"90 Day",
         "task_number":7,
-        "due_date": `${date}`,
+        "due_date": date,
         "task_name": "Foundation and Sewer Hookup Inspection *** must be approved before new tasks to be started ***",
         "isComplete":false,
-        "createdAt": `${createdAt}`
+        "createdAt":  moment().format("LLL")
      
       
-      }]}
+      }
      
 
-  ]);
+    ]);
   // });
 };
