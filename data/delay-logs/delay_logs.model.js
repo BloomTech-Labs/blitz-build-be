1dd4deaded4c06d0209e195fc6f540c205cbfebf
Min-Huang <minh.huang96@gmail.com>
2019-12-05 11:47:13 -0800

    add getLogsByUserId on model.js

--- data/delay-logs/delay_logs.model.js
+++ data/delay-logs/delay_logs.model.js
@@ -3,23 +3,39 @@ const moment = require('moment')
 const createdAt = moment().format("LLL")
 const updatedAt = moment().format("LLL")
 const db = require('../db.config')
-module.exports={
-    getLogs,
-    addLogs,
-    editLogs,
-    deleteLogs
-}
- 
-function getLogs(id){
-    return db("delay_logs").where({id});
+module.exports = {
+  getLogsByUserId,
+  getLogsByLogId,
+  addLogs,
+  editLogs,
+  deleteLogs
+};
+
+function getLogsByUserId(user_id) {
+    return db("delay_logs")
+      .join("projects", "delay_logs.project_id", "=", "projects.id")
+      .join("tasks", "delay_logs.task_id", "=", "tasks.id")
+      .select("projects.project_name", "tasks.task_name", "delay_logs.*")
+      .where("delay_logs.user_id", "=", user_id)
+      .orderBy("delay_logs.id");
 }
 
-function addLogs(newLog){
-    return db("delay_logs").insert(newLog);
+function getLogsByLogId(id) {
+  return db("delay_logs")
+    .where({id})
+    .orderBy("id");
 }
-function editLogs(id,changes){
-  return db("delay_logs").where({id}).update({updatedAt:updatedAt},changes)
+
+function addLogs(newLog) {
+  return db("delay_logs").insert(newLog, "id");
 }
-function deleteLogs(id){
-    return db("delay_logs").where({id}).delete()
+function editLogs(id, changes) {
+  return db("delay_logs")
+    .where({ id })
+    .update(changes);
+}
+function deleteLogs(id) {
+  return db("delay_logs")
+    .where({ id })
+    .del();
 }
