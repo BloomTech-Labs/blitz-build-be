a9a4158f49d4c1e18c28a44c3c31de57fa481ab8
Mike Harley <mharley12345@gmail.com>
2019-12-13 17:10:49 -0800

    .

--- knexfile.js
+++ knexfile.js
@@ -2,28 +2,28 @@
 require("dotenv").config("./env");
 
 module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      host: "localhost",
-      port: 5432,
-      user: process.env.USER,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE,
-      ssl: true
-    },
-      pool: {
-        min: 2,
-        max: 10
-      },
-      migrations: {
-        directory: "./data/migrations",
-        tableName: "knex_migrations"
-      },
-      seeds: {
-        directory: "./data/seeds"
-      }
-  },
+  // development: {
+  //   client: "pg",
+  //   connection: {
+  //     host: "localhost",
+  //     port: 5432,
+  //     user: process.env.USER,
+  //     password: process.env.PASSWORD,
+  //     database: process.env.DATABASE,
+  //     ssl: true
+  //   },
+  //     pool: {
+  //       min: 2,
+  //       max: 10
+  //     },
+  //     migrations: {
+  //       directory: "./data/migrations",
+  //       tableName: "knex_migrations"
+  //     },
+  //     seeds: {
+  //       directory: "./data/seeds"
+  //     }
+  // },
 
 
   production: {
