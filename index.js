62735a2ac33e23e7d8cfe098fe56a31084b48254
Mike Harley <mharley12345@gmail.com>
2019-12-15 11:11:06 -0800

    updates readme

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const server = require("./server");
 
 const PORT = process.env.PORT || 3333;
 
-
+server.use('/',(req,res)=>{
+  res.status(200)
+})
 server.listen(PORT, () => {
   console.log(`\n👾👾👾    blitzbuild\n 👾👾👾 >>> ⎈ ${PORT} <<<`);
 });
