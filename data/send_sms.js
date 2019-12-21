7065d21cb018663984665ae99b0384731908fbd0
Mike Harley <mharley12345@gmail.com>
2019-12-21 13:42:20 -0800

    :construction: testing out sms

--- data/send_sms.js
+++ data/send_sms.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const token = process.env.TOKEN
 const TMClient = require('textmagic-rest-client');
 router.post('/remind',(req,res,next)=>{
-  let phone = +12532057177
+  let phone = '+12532057177'
 var c = new TMClient('michaelharley', token);
 c.Messages.send({text:'This Is Blitz Build Your Tasks Are Do Today', phones:phone}, function(err, res){
     console.log('Messages.send()', err, res);
