46a6cc4605761b83ddc0bc24eb2ed7f4ce58b1b8
Mike Harley <mharley12345@gmail.com>
2020-01-04 21:25:04 -0800

    more updates

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ function logger(req, res, next) {
 
 
 server.use(function(req,res,next){
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Origin", "https://blitz-build-dev.netlify.com")
+  res.header("Access-Control-Allow-Origin", "*");
+
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
